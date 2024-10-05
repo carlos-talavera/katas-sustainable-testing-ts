2f90aece54f097f5de77d68f52361325ca83bcd1
carlos-talavera <carloseduardorivera12@gmail.com>
2024-10-05 09:38:18 -0600

    Change tests description for better readability

--- kata-03-camel-case-converter/src/tests/camel-case-converter.test.ts
+++ kata-03-camel-case-converter/src/tests/camel-case-converter.test.ts
@@ -13,13 +13,13 @@ describe("CamelCaseConverter", () => {
 
         expect(result).toBe(expected);
     });
-    it("should return the words chained in CamelCase format for words separated by spaces with the first letter capitalized", () => {
+    it("should return the words joined in CamelCase format for words separated by spaces with the first letter capitalized", () => {
         const result = toCamelCase("Foo Bar");
         const expected = "FooBar";
 
         expect(result).toBe(expected);
     });
-    it("should return the words chained in CamelCase format for words separated by hyphens with the first letter capitalized", () => {
+    it("should return the words joined in CamelCase format for words separated by hyphens with the first letter capitalized", () => {
         const result = toCamelCase("Foo_Bar-Foo");
         const expected = "FooBarFoo";
 
@@ -31,7 +31,7 @@ describe("CamelCaseConverter", () => {
 
         expect(result).toBe(expected);
     });
-    it("should return the words chained in CamelCase format for lowercase words", () => {
+    it("should return the words joined in CamelCase format for lowercase words", () => {
         const result = toCamelCase("foo_bar foo");
         const expected = "FooBarFoo";
 
