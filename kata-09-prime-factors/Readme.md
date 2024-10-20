62e53b4597936d7a87cdbf36b28a7ea59b5e0b97
carlos-talavera <carloseduardorivera12@gmail.com>
2024-10-20 12:33:11 -0600

    Add kata-09-prime-factors

---
+++ kata-09-prime-factors/Readme.md
@@ -0,0 +1,27 @@
+# Kata 09 - Prime Factors
+
+In this kata, a function that calculates the prime factors of a number is implemented using a TDD approach and [Jest](https://jestjs.io).
+
+## Compile the project
+
+To compile the project, run:
+
+```shell
+npm run compile
+```
+
+## Watch for changes
+
+To make changes and automatically update the build, run:
+
+```shell
+npm run compile:watch
+```
+
+## Run tests
+
+Run the tests:
+
+```shell
+npm run test
+```
---
+++ kata-09-prime-factors/src/tests/prime-factors.test.ts
@@ -0,0 +1,24 @@
+import {getPrimeFactorsFor} from "../core/prime-factors";
+
+describe('The prime factors', () => {
+    it('knows that the first prime is number one', () => {
+        expect(getPrimeFactorsFor(1)).toEqual([1]);
+    });
+
+    it('knows what is a prime number', () => {
+        expect(getPrimeFactorsFor(2)).toEqual([2]);
+        expect(getPrimeFactorsFor(3)).toEqual([3]);
+    });
+
+    it('produces the same result when multiplying the numbers in the output list', () => {
+        expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
+    });
+
+    it('orders the prime factors from the smallest to the biggest', () => {
+        expect(getPrimeFactorsFor(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
+    });
+
+    it('only accepts positive numbers', () => {
+        expect(() => getPrimeFactorsFor(-5)).toThrow();
+    });
+});
